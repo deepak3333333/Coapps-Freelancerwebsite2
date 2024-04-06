aac165eaf0e6bfa96c7e9d027120069525f961c6
deepak3333333 <deepak20wj1a6713@gmail.com>
2024-04-06 12:15:25 +0530

    adding content in header

--- src/components/Navbar/Header/Header.jsx
+++ src/components/Navbar/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className="header">
-      <div className="matter">
+      
         <div className="header-contents">
             <h2>Hire the best <sapn>freelancers for any </sapn>job, online.</h2>
             
@@ -13,9 +13,9 @@ const Header = () => {
 <li>Save up to 90% & get quotes for free</li>
 <li>Pay only when you're 100% happy</li></p>
 
-        </div>
-        <button >Hire a Freelancer</button>
-<button>Earn Money Freelancing</button>
+        
+        <button id="button1">Hire a Freelancer</button>
+<button id="one-time">Earn Money Freelancing</button>
 </div>
       
     </div>
